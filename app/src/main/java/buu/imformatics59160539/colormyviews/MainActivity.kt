5a1d698caa4efea403da9d0056cbde0373dabe40
Kanlaya0709 <59160539@go.buu.ac.th>
2019-08-17 22:16:10 +0700

    Initial project

---
+++ app/src/main/java/buu/imformatics59160539/colormyviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package buu.imformatics59160539.colormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
